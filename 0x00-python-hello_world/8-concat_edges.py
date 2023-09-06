a6d557a915148605bb1cb292fa6564f98dc5dec4
Mycaylo <mkdigrassi@gmail.com>
2023-09-06 09:58:21 -0700

    I've had enough

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming with Python language that combines remarkable power with very clear syntax"
-str = str[39:66]  # Adjusted slice indices
+str = "Python is an interpreted, interactive, object-oriented programming with Python \
+    language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[0:6]
 print(str)
